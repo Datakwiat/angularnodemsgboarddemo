3f2be0d01c45900b74e996bab3347b9c55e12054
David <david.kwiatkowski@team.telstra.com>
2020-06-09 19:25:40 +1000

    messages component template appending the first item from the placeholder data from list

--- frontend/src/app/messages.component.ts
+++ frontend/src/app/messages.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'messages',
-    template: 'this is the messages component'
+    template: 'this is the messages component {{messages[0].text}}'
 })
 export class MessagesComponent {
-    messages = [];
+    messages = [{text:'some text', owner: 'Bob'}, {text: 'more text', owner: 'Jane'}];
 }
