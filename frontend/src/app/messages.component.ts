a5a705031a52a7e5cbd0e31336d8fe198314e899
David <david.kwiatkowski@team.telstra.com>
2020-06-09 19:30:12 +1000

    messages component successfully iterates through entire list of placeholder data

--- frontend/src/app/messages.component.ts
+++ frontend/src/app/messages.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'messages',
-    template: 'this is the messages component {{messages[0].text}}'
+    template: 'this is the messages component <div *ngFor="let message of messages">{{message.text}} by {{message.owner}}</div>'
 })
 export class MessagesComponent {
     messages = [{text:'some text', owner: 'Bob'}, {text: 'more text', owner: 'Jane'}];
