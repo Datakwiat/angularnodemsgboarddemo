69f307486d2d580c65fe6d00e0fdb50a0df1613f
David <david.kwiatkowski@team.telstra.com>
2020-06-10 19:31:27 +1000

    mat cards showing after updating styles.css to include a pre built theme as per https://material.angular.io/guide/theming#using-a-pre-built-theme

--- frontend/src/app/messages.component.ts
+++ frontend/src/app/messages.component.ts
@@ -4,8 +4,12 @@ import { Component } from '@angular/core'
     selector: 'messages',
     template: `
         <div *ngFor="let message of messages">
-            {{message.text}} by: {{message.owner}}
-            <button mat-raised-button>Basic raised button</button>
+        <mat-card style="margin:8px">
+            <mat-card-title> {{message.owner}} </mat-card-title>
+            <mat-card-content>
+                {{message.text}}
+            </mat-card-content>
+        </mat-card>
         </div>
     `
 })
