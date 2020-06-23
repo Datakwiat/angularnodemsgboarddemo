3e7ea4eabb039cd834e5098cc61e72088f48e708
David <david.kwiatkowski@team.telstra.com>
2020-06-23 19:00:26 +1000

    CORS enabled on backend

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,17 @@
 var express = require('express');
 // create express app
 var app = express();
+var bodyParser = require('body-parser');
 
 var messages = [{text:'some text', owner: 'Bob'}, {text: 'more text', owner: 'Jane'}];
+
+// enables CORS
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 
 // create generic route
 app.get('/messages', (req, res) => {
