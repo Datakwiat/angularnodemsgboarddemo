b4001b882aff4c842ed998c250be3fc15bb42766
David <david.kwiatkowski@team.telstra.com>
2020-07-21 20:00:23 +1000

    Post message function created but errors with "ERROR Error: "Uncaught (in promise): HttpErrorResponse"

--- frontend/src/app/web.service.ts
+++ frontend/src/app/web.service.ts
@@ -1,24 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-// Trial Angular Heroes tutorial class
-// class getmsg {
-//     constructor(
-//         public text: string,
-//         public owner: string
-//     ) { }
-// }
-
 @Injectable()
 export class WebService {
-
+    URI = 'http://localhost:1234/api/messages';
     constructor(private http: HttpClient) {}
 
     async getMessages() {
-        let URI = 'http://localhost:1234/api/messages';
-        //return this.http.get(URI).toPromise();
         // TypeScript casting to array is required to extract the object returned
-        return this.http.get<Array<any>>(URI).toPromise();
-        
+        return this.http.get<Array<any>>(this.URI).toPromise();
+    }
+
+    // Take input of message object
+    async postMessage(message) {
+        return this.http.post(this.URI, message).toPromise();
     }
 }
